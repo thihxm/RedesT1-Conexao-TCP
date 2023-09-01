981917b30bac0f761653fa64c5fdba5a1ac0504c
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2023-09-01 11:25:20 -0300

    feat: get server ip + compute hash

--- servidor.py
+++ servidor.py
@@ -4,61 +4,72 @@ import socket
 # import thread module
 from _thread import *
 import threading
+import hashlib
 
 print_lock = threading.Lock()
 
 # thread function
 def threaded(c):
-	while True:
+    while True:
 
-		# data received from client
-		data = c.recv(1024)
-		if not data:
-			print('Bye')
-			
-			# lock released on exit
-			print_lock.release()
-			break
+        # data received from client
+        data = c.recv(1024)
+        if not data:
+            print('Bye')
+            
+            # lock released on exit
+            print_lock.release()
+            break
 
-		# reverse the given string from client
-		data = data[::-1]
+        # reverse the given string from client
+        data = data[::-1]
 
-		# send back reversed string to client
-		c.send(data)
+        # send back reversed string to client
+        c.send(data)
 
-	# connection closed
-	c.close()
+    # connection closed
+    c.close()
 
 
 def Main():
-	host = ""
+    host = ""
 
-	# reserve a port on your computer
-	# in our case it is 12345 but it
-	# can be anything
-	port = 12345
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.bind((host, port))
-	print("socket binded to port", port)
+    # reserve a port on your computer
+    # in our case it is 12345 but it
+    # can be anything
+    port = 12345
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((host, port))
+    hostname = socket.gethostname()
+    ip_address = socket.getfqdn(hostname)
+    print("socket binded to port", port)
+    print(f"Server IP is {ip_address}")
 
-	# put the socket into listening mode
-	s.listen(5)
-	print("socket is listening")
+    # put the socket into listening mode
+    s.listen(5)
+    print(f"socket is listening")
 
-	# a forever loop until client wants to exit
-	while True:
+    # a forever loop until client wants to exit
+    while True:
 
-		# establish connection with client
-		c, addr = s.accept()
+        # establish connection with client
+        c, addr = s.accept()
 
-		# lock acquired by client
-		print_lock.acquire()
-		print('Connected to :', addr[0], ':', addr[1])
+        # lock acquired by client
+        print_lock.acquire()
+        print('Connected to :', addr[0], ':', addr[1])
 
-		# Start a new thread and return its identifier
-		start_new_thread(threaded, (c,))
-	s.close()
+        # Start a new thread and return its identifier
+        start_new_thread(threaded, (c,))
+    s.close()
+    
+def compute_sha256(file_name):
+    hash_sha256 = hashlib.sha256()
+    with open(file_name, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_sha256.update(chunk)
+    return hash_sha256.hexdigest()
 
 
 if __name__ == '__main__':
-	Main()
+    Main()
