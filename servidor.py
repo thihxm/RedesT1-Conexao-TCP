dc78ab01bd01bfe4410d7e578f455c68e891fd98
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2023-09-15 13:50:09 -0300

    feat: protocol parameters generation

--- servidor.py
+++ servidor.py
@@ -1,5 +1,7 @@
 # import socket programming library
 import socket
+from pathlib import Path
+import os
 
 # import thread module
 from _thread import *
@@ -9,26 +11,37 @@ import hashlib
 print_lock = threading.Lock()
 
 # thread function
-def threaded(c):
+def threaded(client):
     while True:
 
         # data received from client
-        data = c.recv(1024)
-        if not data:
+        data = client.recv(1024)
+        if not data or data == b'Sair':
             print('Bye')
             
             # lock released on exit
             print_lock.release()
             break
+        elif data == b'Arquivo':
+            print('Enviando arquivo')
+            file_data = convert_file_to_protocol('./texto.txt')
+            print(file_data)
+        else:
+            print('Reenviando mensagem: ', data)
 
-        # reverse the given string from client
-        data = data[::-1]
-
-        # send back reversed string to client
-        c.send(data)
+            client.send(data)
 
     # connection closed
-    c.close()
+    client.close()
+
+def convert_file_to_protocol(file_path):
+    with open(file_path, 'rb') as file:
+        file_as_path = Path(file.name)
+        file_content = file.read()
+        file_checksum = compute_sha256(file_content)
+        file_name = file_as_path.stem
+        file_size = file_as_path.stat().st_size
+    return file_name, file_size, file_checksum, file_content, 'ok'
 
 
 def Main():
@@ -41,9 +54,11 @@ def Main():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((host, port))
     hostname = socket.gethostname()
-    ip_address = socket.getfqdn(hostname)
+    ip_address = socket.gethostbyname_ex(hostname)
     print("socket binded to port", port)
     print(f"Server IP is {ip_address}")
+    file_data = convert_file_to_protocol('./texto.txt')
+    print(file_data)
 
     # put the socket into listening mode
     s.listen(5)
@@ -53,21 +68,19 @@ def Main():
     while True:
 
         # establish connection with client
-        c, addr = s.accept()
+        client, addr = s.accept()
 
         # lock acquired by client
         print_lock.acquire()
         print('Connected to :', addr[0], ':', addr[1])
 
         # Start a new thread and return its identifier
-        start_new_thread(threaded, (c,))
+        start_new_thread(threaded, (client,))
     s.close()
     
-def compute_sha256(file_name):
+def compute_sha256(data):
     hash_sha256 = hashlib.sha256()
-    with open(file_name, "rb") as f:
-        for chunk in iter(lambda: f.read(4096), b""):
-            hash_sha256.update(chunk)
+    hash_sha256.update(data)
     return hash_sha256.hexdigest()
 
 
